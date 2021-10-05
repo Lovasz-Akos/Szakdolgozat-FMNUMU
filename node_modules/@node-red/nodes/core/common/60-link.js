a9fa4c59c3b9cc901fe4be8c96d1b3d07f5f7df0
Ákos <akos4x44@gmail.com>
2021-10-05 16:17:32 +0200

    Added node-red npm package

---
+++ node_modules/@node-red/nodes/core/common/60-link.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+
+    function LinkInNode(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var event = "node:"+n.id;
+        var handler = function(msg) {
+            msg._event = n.event;
+            node.receive(msg);
+        }
+        RED.events.on(event,handler);
+        this.on("input", function(msg, send, done) {
+            send(msg);
+            done();
+        });
+        this.on("close",function() {
+            RED.events.removeListener(event,handler);
+        });
+    }
+
+    RED.nodes.registerType("link in",LinkInNode);
+
+    function LinkOutNode(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var event = "node:"+n.id;
+        this.on("input", function(msg, send, done) {
+            msg._event = event;
+            RED.events.emit(event,msg)
+            send(msg);
+            done();
+        });
+    }
+    RED.nodes.registerType("link out",LinkOutNode);
+}
