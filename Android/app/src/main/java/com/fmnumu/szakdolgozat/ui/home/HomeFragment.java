29fd8e35c7e1e8f51a9025c71d565f4e6a20a53d
Lovász Ákos <akos4x44@gmail.com>
2022-03-12 18:51:08 +0100

    made home fragment recieve subbed topic messages

--- Android/app/src/main/java/com/fmnumu/szakdolgozat/ui/home/HomeFragment.java
+++ Android/app/src/main/java/com/fmnumu/szakdolgozat/ui/home/HomeFragment.java
@@ -16,14 +16,34 @@ import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.fmnumu.szakdolgozat.MainActivity;
 import com.fmnumu.szakdolgozat.R;
 import com.fmnumu.szakdolgozat.databinding.ActivityMainBinding;
 import com.fmnumu.szakdolgozat.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.w3c.dom.Text;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private final String mqttAddress = "192.168.1.77"; //TODO: put this in storage and make conn fragment change it
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,11 +53,15 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        MqttAndroidClient mqttAndroidClient = ((MainActivity)getActivity()).getClient();
+        if (mqttAndroidClient != null && mqttAndroidClient.isConnected()){
+            mqttNotifier(root, mqttAndroidClient);
+        }
+
+        FloatingActionButton fabSub = root.findViewById(R.id.fabSubscribe);
+        fabSub.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                //TODO: IMPLEMENT ONCLICK SUB
             }
         });
 
@@ -50,6 +74,62 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 
+    private void mqttNotifier(View root, MqttAndroidClient mqttAndroidClient){
 
+        try {
+            if (!mqttAndroidClient.isConnected()) {
+                mqttAndroidClient.connect();
+            }
+            //TODO: move this code to home fragment
+            mqttAndroidClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    Snackbar snackbar = Snackbar
+                            .make(root.findViewById(R.id.snackRoot), "mqtt connection lost", Snackbar.LENGTH_SHORT);
+                    snackbar.setAction("Reconnect", new snackbarReconnectListener());
+                    snackbar.show();
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+                    snackBarMaker(root, "mqtt connection lost");
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+
+                }
+            });
+
+        }
+        catch (MqttException e){
+           snackBarMaker(root, "mqtt connection lost");
+        }
+
+    }
+
+    public class snackbarReconnectListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            MqttAndroidClient client = ((MainActivity)getActivity()).getClient();
+            if (!client.isConnected()) {
+               ((MainActivity)getActivity()).connectMQTT(v);
+                snackBarMaker(v, "mqtt connection lost");
+            }
+
+        }
+    }
+
+    private String decodeMQTT(MqttMessage msg) throws UnsupportedEncodingException {
+        return new String(msg.getPayload(), StandardCharsets.UTF_8);
+    }
+
+    private void snackBarMaker(View view, String message){
+        Snackbar snackbar = Snackbar
+                .make(view.findViewById(R.id.snackRoot), message, Snackbar.LENGTH_SHORT);
+        snackbar.show();
+    }
 
 }
