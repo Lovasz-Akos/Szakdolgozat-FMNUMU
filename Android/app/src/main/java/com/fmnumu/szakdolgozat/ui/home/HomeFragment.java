7f04d530fc598e854ce09fd388881d4d8e1414fd
Lovász Ákos <akos4x44@gmail.com>
2022-03-18 15:05:14 +0100

    removed unused imports

--- Android/app/src/main/java/com/fmnumu/szakdolgozat/ui/home/HomeFragment.java
+++ Android/app/src/main/java/com/fmnumu/szakdolgozat/ui/home/HomeFragment.java
@@ -1,37 +1,25 @@
 package com.fmnumu.szakdolgozat.ui.home;
 
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.cardview.widget.CardView;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.fmnumu.szakdolgozat.MainActivity;
 import com.fmnumu.szakdolgozat.R;
-import com.fmnumu.szakdolgozat.databinding.ActivityMainBinding;
 import com.fmnumu.szakdolgozat.databinding.FragmentHomeBinding;
-import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
