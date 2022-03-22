a50b58e493cbec5e7f53e5a366c740b22b78dd55
Lovász Ákos <akos4x44@gmail.com>
2022-03-22 19:27:47 +0100

    auto code formatting pass

--- Android/app/src/test/java/com/fmnumu/szakdolgozat/ExampleUnitTest.java
+++ Android/app/src/test/java/com/fmnumu/szakdolgozat/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package com.fmnumu.szakdolgozat;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
