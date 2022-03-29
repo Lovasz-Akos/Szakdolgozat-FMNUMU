e1e0a31baad784c629d733ce20548d97e0f701dd
Lovász Ákos <akos4x44@gmail.com>
2022-03-29 19:42:07 +0200

    deleted publish fragment (was used for testing only)

--- Android/app/src/main/java/com/fmnumu/szakdolgozat/MainActivity.java
+++ Android/app/src/main/java/com/fmnumu/szakdolgozat/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_publish, R.id.nav_connection, R.id.nav_settings)
+                R.id.nav_home, R.id.nav_connection, R.id.nav_settings)
                 .setOpenableLayout(drawer)
                 .build();
 
